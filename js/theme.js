b743f80851ec76c19e001469d158703165edb18d
MadEwink <adrien.floriant@telecom-sudparis.eu>
2020-04-13 23:14:05 +0200

    Add 404, improve stuff
    Separate color definition in alternate style css
    	enable browser style change
    	may be changed with a button
    	when the button is used however, browsers won't see different
    		styles anymore until new session
    	the button will not appear if js is blocked by user
    Add simple 404 page

--- js/theme.js
+++ js/theme.js
@@ -1,20 +1,38 @@
 var style_cookie_name = "style";
 
+/*
+ * In order to change style through a button,
+ * if I use the disabled attribute, it prevents browser options
+ * I haven't found a method that does not use this attribute yet
+ */
+
+function activateAlternate(title) {
+  var links = document.getElementsByTagName("link");
+  for (var link of links) {
+    if (!link.hasAttribute("title")) continue;
+    if (link.getAttribute("title") != title)
+      link.disabled = true;
+    else
+      link.disabled = false;
+  }
+}
+
 function setDarkTheme() {
-  document.getElementById('theme').href = "css/style.css";
+  activateAlternate("Dark");
   document.getElementById('light-theme-button').style="display:block";
   document.getElementById('dark-theme-button').style="display:none";
   setStyleCookie("dark");
 }
 
 function setLightTheme() {
-  document.getElementById('theme').href = "css/lightstyle.css";
+  activateAlternate("Light");
   document.getElementById('dark-theme-button').style="display:block";
   document.getElementById('light-theme-button').style="display:none";
   setStyleCookie("light");
 }
 
 function setStyleFromCookie() {
+  setDarkTheme();
   var cookies = document.cookie;
   if (cookies.length == 0) return;
   var array = cookies.split('; ');
@@ -26,7 +44,7 @@ function setStyleFromCookie() {
     if (value_index == -1) return;
     var value = matches[0].substr(value_index+1);
     if (value.localeCompare("light") == 0)
-	  setLightTheme();
+      setLightTheme();
   }
 }
 
