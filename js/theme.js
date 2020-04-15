779eb34397db819406d03e2b1afa4200357f80a2
MadEwink <adrien.floriant@telecom-sudparis.eu>
2020-04-15 07:31:40 +0200

    Fix theme change and persistency

--- js/theme.js
+++ js/theme.js
@@ -6,33 +6,45 @@ var style_cookie_name = "style";
  * I haven't found a method that does not use this attribute yet
  */
 
-function activateAlternate(title) {
+function activateAlternate(title, activate) {
+  var link = document.getElementById(title+"-theme");
+  if (link.getAttribute("title") != title)
+    link.disabled = !activate;
+}
+
+/*
+ * When using the button, the browser sticks to the alternate stylesheet chosen by the user, even when it can't be found because disabled
+ */
+function unAltLinks() {
   var links = document.getElementsByTagName("link");
   for (var link of links) {
+    if (!link.hasAttribute("rel")) continue;
+    if (link.getAttribute("rel") == "alternative stylesheet")
+      link.setAttribute("rel", "stylesheet");
     if (!link.hasAttribute("title")) continue;
-    if (link.getAttribute("title") != title)
-      link.disabled = true;
-    else
-      link.disabled = false;
+    link.removeAttribute("title");
   }
 }
 
 function setDarkTheme() {
-  activateAlternate("Dark");
+  unAltLinks();
+  activateAlternate("Dark", true);
+  activateAlternate("Light", false);
   document.getElementById('light-theme-button').style="display:block";
   document.getElementById('dark-theme-button').style="display:none";
   setStyleCookie("dark");
 }
 
 function setLightTheme() {
-  activateAlternate("Light");
+  unAltLinks();
+  activateAlternate("Light", true);
+  activateAlternate("Dark", false);
   document.getElementById('dark-theme-button').style="display:block";
   document.getElementById('light-theme-button').style="display:none";
   setStyleCookie("light");
 }
 
 function setStyleFromCookie() {
-  setDarkTheme();
   var cookies = document.cookie;
   if (cookies.length == 0) return;
   var array = cookies.split('; ');
@@ -43,11 +55,13 @@ function setStyleFromCookie() {
     var value_index = matches[0].indexOf('=');
     if (value_index == -1) return;
     var value = matches[0].substr(value_index+1);
+    // makes sure there is the light theme button if not theme is set
+    document.getElementById('light-theme-button').style="display:block";
     if (value.localeCompare("light") == 0)
       setLightTheme();
   }
 }
 
 function setStyleCookie(style_name) {
-  document.cookie = style_cookie_name+'='+style_name;
+  document.cookie = style_cookie_name+'='+style_name+"; path=/";
 }
